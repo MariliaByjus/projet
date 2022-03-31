f1224b5608b4ecc832d08cdb39fcba8e7155b90e
MariliaByjus <101297441+MariliaByjus@users.noreply.github.com>
2022-03-30 21:55:58 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var sea;
+
+function preload(){
+  seaImg = loadImage("sea.png");
+  shipImg1 = loadAnimation("ship-1.png, ship-2.png, ship-3.png, ship-4.png");
+
+}
+
+function setup(){
+  createCanvas(400,400);
+sea=createSprite(50,50);
+sea.addImage(seaImg);
+
+}
+
+function draw() {
+  background("blue");
+    drawSprites();
+  
+  }
+
+
